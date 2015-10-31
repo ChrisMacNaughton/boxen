18f575ad5b74ad0852c6558a072c983181d75f66
Chris MacNaughton <chmacnaughton@gmail.com>
2015-10-30 21:07:50 -0400

    update packages

--- modules/people/manifests/chrismacnaughton.pp
+++ modules/people/manifests/chrismacnaughton.pp
@@ -1,21 +1,17 @@
 class people::chrismacnaughton {
   include sublime_text
-  include virtualbox
-  include heroku
+#  include virtualbox
+#  include heroku
   include gcc
   include brewcask
-  include gpgtools
-
+#  include gpgtools
   package { 'adium': provider => 'brewcask' }
   package { 'google-chrome': provider => 'brewcask' }
   package { 'dash': provider => 'brewcask' }
   package { 'hexchat': provider => 'brewcask' }
-
-  # include homebrew
-  # package { 'rust':
-  #   ensure => present,
-  #   provider => 'homebrew'
-  # }
-
-  class { 'gpgtools': }
+  package { 'iterm2': provider => 'brewcask'}
+  package { 'vlc': provider => 'brewcask' }
+  package { 'transmission': provider => 'brewcask' }
+  package { 'dropbox': provider => 'brewcask' } 
+  package { 'gpgtools': provider => 'brewcask' }
 }
