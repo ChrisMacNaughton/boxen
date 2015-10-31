b8398d30b2101e4318d8cb68b2166332f37910cd
Chris MacNaughton <chmacnaughton@gmail.com>
2015-10-30 22:07:05 -0400

    clean up stuff

--- modules/people/manifests/chrismacnaughton.pp
+++ modules/people/manifests/chrismacnaughton.pp
@@ -1,17 +1,5 @@
 class people::chrismacnaughton {
   include sublime_text
-#  include virtualbox
-#  include heroku
   include gcc
   include brewcask
-#  include gpgtools
-  package { 'adium': provider => 'brewcask' }
-  package { 'google-chrome': provider => 'brewcask' }
-  package { 'dash': provider => 'brewcask' }
-  package { 'hexchat': provider => 'brewcask' }
-  package { 'iterm2': provider => 'brewcask'}
-  package { 'vlc': provider => 'brewcask' }
-  package { 'transmission': provider => 'brewcask' }
-  package { 'dropbox': provider => 'brewcask' } 
-  package { 'gpgtools': provider => 'brewcask' }
 }
